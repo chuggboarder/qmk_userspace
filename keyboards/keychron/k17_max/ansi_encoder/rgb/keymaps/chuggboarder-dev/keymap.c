980b5354221a2b5799a5a2ad70448f803a0b9143
chuggboarder <davidh804@gmail.com>
2026-01-01 09:39:54 -0500

    some key assignment

--- keyboards/keychron/k17_max/ansi_encoder/rgb/keymaps/chuggboarder-dev/keymap.c
+++ keyboards/keychron/k17_max/ansi_encoder/rgb/keymaps/chuggboarder-dev/keymap.c
@@ -65,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_BASE] = LAYOUT_104_ansi(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_D,  RGB_MOD,  KC_DEL,  _______,  _______,  LWIN(KC_L),  KC_ENT,
-        KC_DOLLAR,   KC_AMPERSAND,     KC_LBRC,     KC_LCBR,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
-        KC_F13,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,  KC_P4,    KC_P5,    KC_P6,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_F14,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+        KC_DOLLAR,   KC_AMPERSAND,     KC_LBRC,     KC_LCBR,     KC_RCBR,     KC_LBR,     KC_EQL,     KC_ASTR,     KC_RBR,     KC_PLUS,     KC_RBRC,     KC_EXCLAIM,    KC_HASH,   KC_BSPC,            KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,   KC_SCLN,     KC_COMM,     KC_DOT,     KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLS,    KC_AT,  KC_BSLS,            KC_PGDN,  KC_P1,    KC_P2,    KC_P3,    KC_PPLS,
+        KC_F13,  KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,  KC_MINS,              KC_ENT,             KC_HOME,  KC_P4,    KC_P5,    KC_P6,
+        KC_LSFT,            KC_QUOT,     KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,  KC_V,   KC_Z,              KC_RSFT,  KC_F14,              KC_P7,    KC_P8,    KC_P9,    KC_PENT,
         KC_F15,  KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RCTL,  KC_RALT, KC_F16,  KC_LWIN,  KC_APP,  MO(WIN_FN),  KC_P0,              KC_PDOT         ),
 
     [WIN_FN] = LAYOUT_104_ansi(
@@ -78,6 +78,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            KC_END,   _______,  _______,  _______,
         _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,            _______,  _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  KC_RWIN,  KC_MENU,  _______,  _______,            _______         ),
+
+
+
+    /* ---shift layer---
+    
+    [WIN_SHIFT] = LAYOUT_104_ansi(
+        _______,   _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______,     _______,   KC_D,  RGB_MOD,  KC_DEL,  _______,  _______,  LWIN(KC_L),  KC_ENT,
+        KC_DOLLAR,   KC_AMPERSAND,     KC_LBRC,     KC_LCBR,     KC_RCBR,     KC_LBR,     KC_EQL,     KC_asterisk,     KC_RBR,     KC_PLUS,     KC_RBRC,     KC_EXCLAIM,    KC_HASH,   KC_BSPC,            KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,   KC_SCLN,     KC_COMM,     KC_DOT,     KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLS,    KC_AT,  KC_BSLS,            KC_PGDN,  KC_P1,    KC_P2,    KC_P3,    KC_PPLS,
+        KC_F13,  KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,  KC_MINS,              KC_ENT,             KC_HOME,  KC_P4,    KC_P5,    KC_P6,
+        KC_LSFT,            KC_QUOT,     KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,  KC_V,   KC_Z,              KC_RSFT,  KC_F14,              KC_P7,    KC_P8,    KC_P9,    KC_PENT,
+        KC_F15,  KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RCTL,  KC_RALT, KC_F16,  KC_LWIN,  KC_APP,  MO(WIN_FN),  KC_P0,              KC_PDOT         ),
+
+
+*/
+
+    
+    
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -99,8 +117,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 
+
+
+/* hardware reset: "hold down h and ' and Fn at the same time" */
+
+
 /* Local Variables: */
 /* compile-command: "qmk flash" */
 /* End: */
 
-/* (browse-url "http://localhost:8000/") */
